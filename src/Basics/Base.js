f3f4cd83aefcb5796047fd8d52ee16ec7b5e17db
Jinxt <jinkxt@gmail.com>
2022-05-11 16:04:46 +0200

    Added Bootstrap navigation bar

--- src/Basics/Base.js
+++ src/Basics/Base.js
@@ -1,11 +1,15 @@
 import Footer from "./Footer";
 import Header from "./Header";
+import Header2 from "./Header2";
 
 function Base() {
   return (
     <>
+      <Header2 />
       <Header />
+      
       <Footer />
+      
     </>
   );
 }
