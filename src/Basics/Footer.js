d6aa92294ec63602a8566f47ecd6a947a94d7d7d
Jinxt <jinkxt@gmail.com>
2022-05-11 14:37:15 +0200

    Basics (Header/Footer)

---
+++ src/Basics/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+      <footer className="mt-auto py-5 bg-dark">
+        <div className="container d-flex justify-content-center">
+          <span className="text-muted">Copyright &copy; Website 2022, za3ma</span>
+        </div>
+      </footer>
+    );
+  }
+  
+  export default Footer;
---
+++ src/Basics/Header.js
@@ -0,0 +1,12 @@
+import logo from '../logo.svg';
+function Header() {
+    return (
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                Welcome to DZ zone
+            </p>
+        </header>  
+    );
+  }
+  export default Header;
