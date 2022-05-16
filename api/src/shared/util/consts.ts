76f6ad920fecdd33c710f8b384fb867b3fd391f4
Nacereddine <n.karce@pharmainvest.dz>
2022-05-16 18:19:54 +0100

    add dummy products

--- api/src/shared/util/consts.ts
+++ api/src/shared/util/consts.ts
@@ -1,4 +1,60 @@
 const isProduction = process.env.NODE_ENV === 'production';
+const popularProducts = [
+  {
+    id: 1,
+    img: 'https://www.dpsmarket.com/230-large_default/algerienne-nawhal-s-950ml.jpg',
+  },
+  {
+    id: 2,
+    img: 'https://boucherie-excellence.fr/wp-content/uploads/2020/12/Sauce_algerienne.png',
+  },
+  {
+    id: 3,
+    img: 'http://alaksafood.com/images/ban1_elem8.png',
+  },
+  {
+    id: 4,
+    img: 'https://www.lemarchetunisien.com/new/images/Products/HMISALGERIENSANNEFA.png',
+  },
+  {
+    id: 5,
+    img: 'https://www.taybadates.com/wp-content/uploads/2020/11/DATES-ON-BRANCH.png',
+  },
+  {
+    id: 6,
+    img: 'https://alamirahalva.com/wp-content/uploads/2022/02/150gr_Amande.png',
+  },
+  {
+    id: 7,
+    img: 'http://carajus.com/wp-content/uploads/2019/09/Produit-cara-tomatesz-rechrach-dct-400g-ar.png',
+  },
+  {
+    id: 8,
+    img: 'https://safpalideal.com/wp-content/uploads/2018/05/safpal_ideal_produits_flan_banane_500g_512x512.png',
+  },
+];
 
-
-export { };
+const sliderItems = [
+  {
+    id: 1,
+    img: 'https://www.hamoud-boualem.com/media/k2/items/cache/077ab55046ce80eaf9a3ddea999597ca_XL.png',
+    title: 'Hamoud Boualem',
+    desc: "DON'T COMPROMISE, GET THE ABSOLUTE BEST.",
+    bg: 'f5fafd',
+  },
+  {
+    id: 2,
+    img: 'https://i0.wp.com/www.algeriepart.com/wp-content/uploads/2021/02/huile-dz-.png?fit=500%2C400&ssl=1',
+    title: 'Elio ',
+    desc: 'Hada ezzit wella battel.',
+    bg: 'fcf1ed',
+  },
+  {
+    id: 3,
+    img: 'https://img.cuisineaz.com/680x357/2019/07/17/i148153-.jpeg',
+    title: 'lkhobz',
+    desc: 'wellah twa7echt l  matlou3.',
+    bg: 'fbf0f4',
+  },
+];
+export { popularProducts, sliderItems };
